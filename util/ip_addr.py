7f5565c9d7f7cde58fec1e55104e18d6af0ebb15
Shamil-G <s.gusseynov@gmail.com>
2025-07-11 09:07:57 +0500

    Изменен файл court.conf - добавлены нужные строки в заголовок proxy_set_header

--- util/ip_addr.py
+++ util/ip_addr.py
@@ -4,8 +4,6 @@ from main_app import log
 
 def ip_addr():
     if platform=='unix':
-        log.info(f"UNIX. IP_ADDR: {request.environ.get(http_ip_context, '')}\n\http_ip_context: {http_ip_context}t")
         return request.environ.get(http_ip_context, '')
     else:
-        log.info(f"NOT UNIX. IP_ADDR: {request.environ.get(http_ip_context, '')}")
         return request.remote_addr
