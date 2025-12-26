dd258e1f2c67ba3775bc63e17ec1b9291f6359bf
Shamil-G <s.gusseynov@gmail.com>
2025-12-26 14:46:06 +0500

    COMMIT on Server

---
+++ init.bat
@@ -0,0 +1,21 @@
+#python3 -m venv venv
+
+. /home/court/GFSSCourt/venv/bin/activate
+#call C:\Projects\Aktuar_2021\venv\Scripts\activate.bat
+
+python -m pip install --upgrade pip
+
+pip install --upgrade pip
+#pip install gevent
+#pip install flask
+#pip install flask_login
+#pip install flask_session
+#pip install xlsxwriter
+#pip install gunicorn
+#pip install requests
+#pip install oracledb
+#pip install redis
+#pip install flask_session
+pip install pandas
+pip install xlsxwriter
+#gunicorn
